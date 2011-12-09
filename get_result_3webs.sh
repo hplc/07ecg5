ee18880021a20ed7507a1ddccf89589f5a5df9be
Sunry <sunrychen@gmail.com>
2011-12-09 11:15:58 +0800

    first commit

---
+++ get_result_3webs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48
+do
+# echo 192.168.61.$i
+wget --tries 1 --timeout 2 http://www$i.baidu.com/php.php
+wget --tries 1 --timeout 2 http://www$i.baidu.com:81/asp.asp
+wget --tries 1 --timeout 2 http://www$i.baidu.com:82/jsp.jsp
+done
---
+++ index.jsp
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+var i=0;
+for (i=0;i<=50;i++)
+{
+alert("Hello!\n2011.05.05");
+}
+</script>
